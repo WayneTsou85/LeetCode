e46bd4ad9a231c5076560c3abb41191ce932f4f0
Wayne Tsou <wayne850109@gmail.com>
2024-01-13 13:34:08 +0800

    :recycle: Refactor 342

--- src/main/java/easy/IsSubsequence.java
+++ src/main/java/easy/IsSubsequence.java
@@ -2,7 +2,7 @@ package easy;
 
 public class IsSubsequence {
     public boolean isSubsequence(String s, String t) {
-        if (s.length() == 0) return true;
+        if (s.isEmpty()) return true;
         char[] refChar = s.toCharArray();
         int refIndex = 0;
         for (char c : t.toCharArray()) {
